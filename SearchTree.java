bbeeaa1491c4da93758cdbec60ce1f41638d229f
AnttonLA <35562017+AnttonLA@users.noreply.github.com>
2018-02-14 15:58:42 +0000

    Add files via upload

--- SearchTree.java
+++ SearchTree.java
@@ -1,21 +1,31 @@
-
+import java.util.ArrayList;
 
 public class SearchTree {
 	private Node root;
 	private String role;
-	//private int bestValue;
+	eval = new EvalFunction(this.width,this.height);
 	
 	public SearchTree(Node r, String role) {
 		this.role = role;
 		root = r;
 	}
 	
-	public int negaMax(Node n) {
+	public int negaMax(int depth, Node n, String r) {
+		ArrayList<String> moves = new ArrayList<String>();
+		String r2;
 		if (n.getState().testIfTerminal()) {
-			return n.getState().whitePawns;
+			return 2;
 		}
 		int bestValue = Integer.MIN_VALUE;
-		n.getState().getLegalMoves(role);
+		if (r.equals("black")) {
+			r2 = "white";
+		} else {
+			r2 = "black";
+		}
+		moves = n.getState().getLegalMoves(r2);
+		for (int i = 0; i < moves.size(); i++) {
+			//Create an new Node and call MiniMax again!
+		}
 		
 	}
 }
